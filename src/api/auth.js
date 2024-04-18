3c278b163e35275d5927c5ec16a33c7a022419df
Ram Krishan <xeam.designer7@gmail.com>
2024-04-18 18:23:50 +0530

    user page list users

--- src/api/auth.js
+++ src/api/auth.js
@@ -5,3 +5,37 @@ export const login = (data) => API.request({
     method: "POST",
     data
 });
+
+export const usersList = (data) => {
+    const token = localStorage.getItem('token');
+    return API.request({
+        url: 'user/all',
+        method: "GET",
+        data,
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    })
+};
+
+export const CompaniesList = (id) => {
+    const token = localStorage.getItem('token');
+    return API.request({
+        url: `company/user/${id}`,
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    })
+};
+export const CompaniesDelete = (id) => {
+    const token = localStorage.getItem('token');
+    return API.request({
+        url: `company/${id}`,
+        method: "DELETE",
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    })
+};
+
