491ba528947e7e75ebc2b9ead9d3807ab1ac7d2b
Ram Krishan <xeam.designer7@gmail.com>
2024-04-29 18:24:45 +0530

    dashboard ui design table

--- src/components/CustomDashboard.js
+++ src/components/CustomDashboard.js
@@ -64,59 +64,59 @@ function CustomDashboard({
 
   return (
     <div className="layout-wrapper layout-content-navbar">
-      <div className="layout-container">
-
-        <aside id="layout-menu" className="layout-menu menu-vertical menu bg-menu-theme">
-          <div className="app-brand demo">
-            <Link href="/dashboard" className="app-brand-link">
-              <span className="app-brand-logo dashboard">
-                <img src="/INVO.svg" />
-              </span>
-            </Link>
-
-            <a className="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
-              <i className="bx bx-chevron-left bx-sm align-middle"></i>
-            </a>
-          </div>
-
-          <div className="menu-inner-shadow"></div>
-
-          <ul className="menu-inner py-1">
-
-            {dashboardItems.map((item,  index) => {
-              return (
-                <li key={`item${index}`} className={(pathname == item?.pathName ? 'active' : '') + ' menu-item'}>
-                <Link href={item?.pathName} className="menu-link">
-                    {item?.icon}
-                    <div data-i18n="Dashboards">{item?.title}</div>
+        <div className="layout-container p-3">
+          <div className='layout-left'>
+              <aside id="layout-menu" className="layout-menu menu-vertical menu bg-menu-theme">
+                <div className="app-brand demo">
+                  <Link href="/dashboard" className="app-brand-link">
+                    <span className="app-brand-logo dashboard">
+                      <img src="/INVO.svg" />
+                    </span>
                   </Link>
-                </li>
-              );
-            })}            
 
-            <li className="menu-item">
-              <Link href={'/'} onClick={(e) => { logoutUser(e)}} className="menu-link">
-                <FeatherIcon icon="log-out" className='menu-icon' />
-                <div data-i18n="Dashboards">Logout</div>
-              </Link>
-            </li>
+                  <a className="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+                    <i className="bx bx-chevron-left bx-sm align-middle"></i>
+                  </a>
+                </div>
 
-          </ul>
-        </aside>
+                <div className="menu-inner-shadow"></div>
 
-        <div className="layout-page">
+                <ul className="menu-inner py-1">
 
-          <div className="content-wrapper">
+                  {dashboardItems.map((item,  index) => {
+                    return (
+                      <li key={`item${index}`} className={(pathname == item?.pathName ? 'active' : '') + ' menu-item'}>
+                      <Link href={item?.pathName} className="menu-link">
+                          {item?.icon}
+                          <div data-i18n="Dashboards">{item?.title}</div>
+                        </Link>
+                      </li>
+                    );
+                  })}            
 
-            <div className="container-xxl flex-grow-1 container-p-y">
-              <div className='row'>
-                {children}
-              </div>
-            </div>
+                  <li className="menu-item">
+                    <Link href={'/'} onClick={(e) => { logoutUser(e)}} className="menu-link">
+                      <FeatherIcon icon="log-out" className='menu-icon' />
+                      <div data-i18n="Dashboards">Logout</div>
+                    </Link>
+                  </li>
 
-            <div className="content-backdrop fade"></div>
+                </ul>
+              </aside>
           </div>
-        </div>
+          <div className="layout-page">
+
+            <div className="content-wrapper">
+
+            <div className="container" style={{paddingRight:"10px"}}>
+                <div className='row'>
+                  {children}
+                </div>
+              </div>
+
+              <div className="content-backdrop fade"></div>
+            </div>
+          </div>
       </div>
 
       <div className="layout-overlay layout-menu-toggle"></div>
